405e2c3d7316c95e602984e5dbcdc0a2eb971e49
ABravoCode <jeremyan@connect.hku.hk>
2023-10-25 18:44:04 +0800

    Ignore CMakes

---
+++ Helloworld/main.cpp
@@ -0,0 +1,10 @@
+// Date: 25 Oct 2023
+// Author: Jeremy An
+// E-mail: jeremyse0714 at gmail dot com
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
